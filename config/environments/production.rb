36a3562d2d09f8e672286a74fd1b52fc44a041fa
Virginia <vcooper_us@yahoo.com>
2015-09-21 15:08:18 -0700

    Updated production.rb again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.default_url_options = {host: 'vrcooper-blocitoff.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'vrcooper-blocitoff.herokuapp.com'}
 end
