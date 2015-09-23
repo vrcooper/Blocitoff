3dd635c92d32be32323611bf9761c49cc7e0210e
Virginia <vcooper_us@yahoo.com>
2015-09-23 00:50:26 -0700

    Second failed attempt at redirecting to show page with appropriate user details.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
 before_action :authenticate_user!, except: [:show]
 
 
-  def update
+  def show
+    @user = User.find(params[:id])
+  end
+
+ def update
     if current_user.update_attributes(user_params)
       flash[:notice] = "User information updated"
       redirect_to edit_user_registration_path
@@ -11,11 +15,6 @@ before_action :authenticate_user!, except: [:show]
       flash[:error] = "Invalid user information"
       redirect_to edit_user_registration_path
     end
-  end
-
-
-  def show
-    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'users/show'
 
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:update, :show]
   get 'welcome/index'
 
   root to: 'welcome#index'
