69e4149f801bbf7c066d16633e0c60dca60b5fbd
Virginia <vcooper_us@yahoo.com>
2015-09-24 00:36:05 -0700

    Unsuccessful attempt at redirecting to user show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,7 @@ before_action :authenticate_user!, except: [:show]
 
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
- def update
-    if current_user.update_attributes(user_params)
-      flash[:notice] = "User information updated"
-      redirect_to edit_user_registration_path
-    else
-      flash[:error] = "Invalid user information"
-      redirect_to edit_user_registration_path
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :avatar)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
+  
 
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:show]
+  
   get 'welcome/index'
 
+
   root to: 'welcome#index'
+
 
 end
