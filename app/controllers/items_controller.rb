6b1320177ab5ba8ee833b4f486cdac13f39fbfd5
Virginia <vcooper_us@yahoo.com>
2015-09-28 12:49:27 -0700

    updated items_controller and items partials.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,11 @@ class ItemsController < ApplicationController
 
 
   def create
-      @user = User.find(params[:user_id])
+      @user = current_user
       @items = @user.items
 
       @item = Item.new  
-      @item = current_user
+      @item.user = current_user
      
 
      if @item.save
